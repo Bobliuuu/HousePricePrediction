9159dded5da828c9f37ff5b2a2373fc19c0714b3
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-05-15 17:38:34 -0400

    Fixed streamlit navbar and templating

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
-from operator import indexOf
 import streamlit as st
-import utils as utl
-from pages import index, problem, cleaning, processing, analysis, polate, advanced, prediction, conclude, references, code
 
-st.set_page_config(layout="wide", page_title='Navbar sample')
-st.set_option('deprecation.showPyplotGlobalUse', False)
-utl.inject_custom_css()
-utl.navbar_component()
+a = st.selectbox("View Page: ", ["index", "problem", "cleaning", "processing", "analysis", "polate", "advanced", "prediction"
+                                 "conclude", "references", "code"])
 
-def navigation():
-    route = utl.get_current_route()
-    if route == "problem":
-        problem.load_view()
-    elif route == "cleaning":
-        cleaning.load_view()
-    elif route == "processing":
-        processing.load_view()
-    elif route == "analysis":
-        analysis.load_view()
-    elif route == "polate":
-        polate.load_view()
-    elif route == "advanced":
-        advanced.load_view()
-    elif route == "prediction":
-        prediction.load_view()
-    elif route == "conclude":
-        conclude.load_view()
-    elif route == "references":
-        references.load_view()
-    elif route == "code":
-        code.load_view()
-    elif route == None:
-        index.load_view()
-        
-navigation()
+if a == "problem":
+    pass
+elif a == "cleaning":
+    
+elif a == "processing":
+    
+elif a == "analysis":
+    
+elif a == "polate":
+    
+elif a == "advanced":
+    
+elif a == "prediction":
+    
+elif a == "conclude":
+    
+elif a == "references":
+    
+elif a == "code":
+    
+elif a == None:
+    
