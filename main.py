cfcad519e947c30db8369e41a8da3da9036431cc
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-05-15 13:33:12 -0400

    Updated filesystem
    Updated dependencies for app routing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from operator import indexOf
 import streamlit as st
 import utils as utl
-from pages import home, about, analysis, options, configuration
+from pages import problem, cleaning, processing, analysis, polate, advanced, prediction, conclude, references, code
 
 st.set_page_config(layout="wide", page_title='Navbar sample')
 st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -10,16 +11,26 @@ utl.navbar_component()
 def navigation():
     route = utl.get_current_route()
     if route == "problem":
-        home.load_view()
-    elif route == "about":
-        about.load_view()
+        problem.load_view()
+    elif route == "cleaning":
+        cleaning.load_view()
+    elif route == "processing":
+        processing.load_view()
     elif route == "analysis":
         analysis.load_view()
-    elif route == "options":
-        options.load_view()
-    elif route == "configuration":
-        configuration.load_view()
+    elif route == "polate":
+        polate.load_view()
+    elif route == "advanced":
+        advanced.load_view()
+    elif route == "prediction":
+        prediction.load_view()
+    elif route == "conclude":
+        conclude.load_view()
+    elif route == "references":
+        references.load_view()
+    elif route == "code":
+        code.load_view()
     elif route == None:
-        home.load_view()
+        indexOf.load_view()
         
 navigation()
