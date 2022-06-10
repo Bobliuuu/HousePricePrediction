d4caddfb8aa7fe98fc53287ddec1d6d0b24d3b5f
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-06-10 13:49:42 -0400

    Add model training

--- main.py
+++ main.py
@@ -1,19 +1,81 @@
+from asyncore import write
+import code
+from matplotlib.pyplot import title
 import streamlit as st
+import math
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import ElasticNet, Lasso
+from sklearn.kernel_ridge import KernelRidge
+import xgboost as xgb
+import lightgbm as lgb
+from sklearn.pipeline import make_pipeline
+import pickle
 
 a = st.selectbox("View Page: ", ["Intro", "Linear Regression", "Quadratic Regression", "Exponential Regression", "Machine Learning"])
 
 if a == "Linear Regression":
     st.title('Linear Regression')
     st.write('A linear regression model was created based on the sample data.')
-    st.latex("$y_1 \sim mx_1$ + b ")
+    st.latex("y_1 \sim ax_1 + b ")
     x = int(st.number_input('Size of Living Area', 0, 1000000, 0, 1))
     st.write('Price of House: ' + str(60.40067407*x + 80101.1986))
 elif a == "Quadratic Regression":
-    st.title('"Quadratic Regression')
+    st.title('Quadratic Regression')
+    st.title('Linear Regression')
+    st.write('A quadratic regression model was created based on the sample data.')
+    st.latex("y_1 \sim a{x_1}^2 + bx_1 + c ")
+    x = int(st.number_input('Size of Living Area', 0, 1000000, 0, 1))
+    st.write('Price of House: ' + str(-13459 + 168*x + -0.026*math.pow(x, 2)))
 elif a == "Exponential Regression":
     st.title('Exponential Regression')
+    st.title('Linear Regression')
+    st.write('A exponential regression model was created based on the sample data.')
+    st.latex("y_1 \sim ae^{bx_1} ")
+    x = int(st.number_input('Size of Living Area', 0, 1000000, 0, 1))
+    st.write('Price of House: ' + str(103852*exp(0.000348*x))
 elif a == "Machine Learning":
     st.title('Machine Learning')
+    st.title('A number of machine learning models were trained on the dataset, in a Google Colab notebook.')
+    with open("notebooks/Machine_Learning.ipynb", "rb") as file:
+        btn = st.download_button(
+            label="Download Machine Learning Notebook",
+            data=file,
+            file_name="Machine_Learning.ipynb",
+            mime="application/octet-stream" # Converts file data to byte stream, parsing to cout
+        )
+    st.write("From an analysis of the machine learning models, (see report), we decided to use the ensemble prediction method,",
+    "with a stacked averaging meta-model, and XGBoost and Light GBM gradient boosting.")
+    st.code("stacked_models = StackingAveragedModels(base_models = (enet_model, xgb_model, kri_model),"+
+                                                 "meta_model = lasso_model")
+    st.code("xgb_model = xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468,"+
+                             "learning_rate=0.05, max_depth=3,"+
+                             "min_child_weight=1.7817, n_estimators=2200,"+
+                             "reg_alpha=0.4640, reg_lambda=0.8571,"+
+                             "subsample=0.5213, silent=1,"+
+                             "random_state=7, nthread=-1")
+    st.code("lgb_model = lgb.LGBMRegressor(objective='regression',num_leaves=5,"
+                              "learning_rate=0.05, n_estimators=720,"+
+                              "max_bin = 55, bagging_fraction = 0.8,"+
+                              "bagging_freq = 5, feature_fraction = 0.2319,"+
+                              "feature_fraction_seed=9, bagging_seed=9,"+
+                              "min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)")
+    st.code("# After training each individual model\nensemble = stacked_pred*0.50 + xgb_pred*0.25 + lgb_pred*0.25")
+    st.write("This code gave us the lowest RMSLE score of 1.0107.")
+
+    uploaded_file = st.file_uploader("Upload Testing Dataset")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.write(dataframe)
+
+    with open('ensemble.pkl', 'rb') as f:
+        ensemble = pickle.load(f)
+
+    result = pd.DataFrame()
+    result['Id'] = df['Id']
+    result['SalePrice'] = ensemble
+    sub.to_csv('result.csv', index=False)
+    
 elif a == "Intro":
     st.title('House Price Prediction 🏘️🏙️📈📉')
     st.write('A web application demonstrating the results and conclusions of the House Price Prediction report.')
