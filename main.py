df93c7588e6619a9ae78a20f668079f2d2ea3a49
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-06-14 23:06:16 -0400

    Fixed more typos

--- main.py
+++ main.py
@@ -53,12 +53,12 @@ elif a == "Machine Learning":
                              "random_state=7, nthread=-1")
     st.code("lgb_model = lgb.LGBMRegressor(objective='regression',num_leaves=5,"
                               "learning_rate=0.05, n_estimators=720,"+
-                              "max_bin = 55, bagging_fraction = 0.8,"+
-                              "bagging_freq = 5, feature_fraction = 0.2319,"+
+                              "max_bin=55, bagging_fraction=0.8,"+
+                              "bagging_freq=5, feature_fraction=0.2319,"+
                               "feature_fraction_seed=9, bagging_seed=9,"+
-                              "min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)")
+                              "min_data_in_leaf=6, min_sum_hessian_in_leaf=11)")
     st.code("# After training each individual model\nensemble = stacked_pred*0.50 + xgb_pred*0.25 + lgb_pred*0.25")
-    st.write("This code gave us the lowest RMSLE score of 1.0107.")
+    st.write("This code gave us the lowest RMSLE score of 1.1009.")
 
     with open('models/ensemble.pkl', 'rb') as f:
         ensemble = pickle.load(f)
